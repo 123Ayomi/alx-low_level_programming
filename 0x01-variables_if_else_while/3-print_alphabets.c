c949de1241c3d08fe8a0df702968e8bbd24905eb
123Ayomi <otepolabusayo@yahoo.com>
2023-06-16 14:31:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Print the alphabet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ";
+int i;
+for (i = 0; 1 < 52; 1++)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
