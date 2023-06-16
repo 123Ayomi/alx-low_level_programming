da8b1f36dc4d3179cc7c6f3c5ea88ffa026a798b
123Ayomi <otepolabusayo@yahoo.com>
2023-06-16 15:18:23 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
+
 /**
-* main - Print the alphabet
+* main - Print the alphabe
+* 
 * Return: Always 0 (Success)
+
 */
 int main(void)
 {
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUWXYZ";
-int i;
-for (i = 0; 1 < 52; 1++)
+int lowerCase = 'a';
+int upperCase = 'A';
+while (lowercase <= 'z')
 {
-putchar(alp[i]);
+putchar(lowerCase);
+lowerCase += 1;
+}
+while (upperCase <= 'z')
+{
+putcher(upperCase)
+upperCase += 1;	
 }
 putchar('\n');
 return (0);
