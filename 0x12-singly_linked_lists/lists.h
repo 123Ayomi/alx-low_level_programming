14e453dd2bfc5a188d938e4726c9b1c4d0057c11
123Ayomi <otepolabusayo@yahoo.com>
2023-07-27 16:12:48 -0700

    lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,12 +2,12 @@
 #define _LINKED_LIST
 
 /**
-* struct list_s - singly linked list
+* struct list_s - The singly linked list
 * @str: string - (malloc'ed string)
-* @len: length of the string
-* @next: points to the next node
-* Description: singly linked list node structure
-* for alx project
+* @len: A length of the string
+* @next: The points to the next node
+* Description: A singly linked list node structure
+* for alx project Assignment
 */
 
 typedef struct list_s
@@ -17,11 +17,11 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 
-int _strlen(const char *s);
-size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+int _strlen(const char *s);
+size_t print_list(const list_t *h);
 
 #endif
