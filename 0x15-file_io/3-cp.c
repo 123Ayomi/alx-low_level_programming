04339882459c86693edd4c08ea7dc11522d2d6fe
123Ayomi <otepolabusayo@yahoo.com>
2023-08-07 18:38:31 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -72,54 +72,80 @@ int main(int argc, char *argv[])
 
 {
 
-int f, l, r, w;
+int from, to, r, w;
 
-char *buffer;
-
-
-if (argc != 3)
-
-{
-
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-
-exit(97);
-
-}
+        char *buffer;
 
 
-buffer = create_buffer(argv[2]);
+        if (argc != 3)
 
-f = open(argv[1], O_RDONLY);
+        {
 
-r = read(from, buffer, 1024);
+                dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 
-l = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+                exit(97);
+
+        }
 
 
-do {
+        buffer = create_buffer(argv[2]);
 
-if (f == -1 || r == -1)
+        from = open(argv[1], O_RDONLY);
 
-{
-dprintf(STDERR_FILENO,
-"Error: Can't read from file %s\n", argv[1]);
-free(buffer);
-exit(98);
-}
-w = write(l, buffer, r);
-if (l == -1 || w == -1)
-{
-dprintf(STDERR_FILENO,
-"Error: Can't write to %s\n", argv[2]);
-free(buffer);
-exit(99);
-}
-r = read(f, buffer, 1024);
-l = open(argv[2], O_WRONLY | O_APPEND);
-} while (r > 0);
-free(buffer);
-close_file(f);
-close_file(l);
-return (0);
+        r = read(from, buffer, 1024);
+
+        to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+
+        do {
+
+                if (from == -1 || r == -1)
+
+                {
+
+                        dprintf(STDERR_FILENO,
+
+                                "Error: Can't read from file %s\n", argv[1]);
+
+                        free(buffer);
+
+                        exit(98);
+
+                }
+
+
+                w = write(to, buffer, r);
+
+                if (to == -1 || w == -1)
+
+                {
+
+                        dprintf(STDERR_FILENO,
+
+                                "Error: Can't write to %s\n", argv[2]);
+
+                        free(buffer);
+
+                        exit(99);
+
+                }
+
+
+                r = read(from, buffer, 1024);
+
+                to = open(argv[2], O_WRONLY | O_APPEND);
+
+
+        } while (r > 0);
+
+
+        free(buffer);
+
+        close_file(from);
+
+        close_file(to);
+
+
+        return (0);
+
 }
